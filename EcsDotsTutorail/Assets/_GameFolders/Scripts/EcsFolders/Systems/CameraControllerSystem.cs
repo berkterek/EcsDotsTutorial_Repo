eaf600fc3a41b87334c81ad854cbd791b7cb7149
berkterek <42406263+berkterek@users.noreply.github.com>
2023-07-18 17:26:02 +0300

    feat: game camera can make rotation

---
+++ EcsDotsTutorail/Assets/_GameFolders/Scripts/EcsFolders/Systems/CameraControllerSystem.cs
@@ -0,0 +1,34 @@
+﻿using EcsDotsTutorial.Components;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace EcsDotsTutorial.Systems
+{
+    public partial class CameraControllerSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            var brainEntity = SystemAPI.GetSingletonEntity<BrainTag>();
+            var brainScale = SystemAPI.GetComponent<LocalTransform>(brainEntity).Scale;
+
+            var cameraSingleton = CameraController.Instance;
+
+            if (cameraSingleton == null) return;
+
+            var positionFactor = (float)SystemAPI.Time.ElapsedTime * cameraSingleton.Speed;
+            
+            var radius =cameraSingleton.RadiusAtScale(brainScale);
+            var height = cameraSingleton.HeightAtScale(brainScale);
+
+            cameraSingleton.transform.position = new Vector3()
+            {
+                x = Mathf.Cos(positionFactor) * radius,
+                y = height,
+                z = Mathf.Sin(positionFactor) * radius
+            };
+            
+            cameraSingleton.transform.LookAt(Vector3.zero);
+        }
+    }
+}
---
+++ EcsDotsTutorail/Assets/_GameFolders/Scripts/MonoFolders/Concretes/Controllers/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float _startRadius;
+    [SerializeField] float _endRadius;
+    [SerializeField] float _startHeight;
+    [SerializeField] float _endHeight;
+    [SerializeField] float _speed;
+    
+    public static CameraController Instance { get; private set; }
+
+    public float RadiusAtScale(float scale) => Mathf.Lerp(_startRadius, _endRadius, 1f - scale);
+    public float HeightAtScale(float scale) => Mathf.Lerp(_startHeight, _endHeight, 1f - scale);
+    public float Speed => _speed;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
